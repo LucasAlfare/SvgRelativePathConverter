d4496c3a90e52f734b1bcb875a0569dfed4af846
Lucas Sousa <god-lucas@hotmail.com>
2019-03-01 15:41:05 -0300

    Adicionado classe principal para testes via parâmetro

--- src/Testes.java
+++ src/Testes.java
@@ -4,8 +4,8 @@ public class Testes {
         //converte um path relativo que tenha sido passado via parâmetro
         if (args.length != 0) {
             String s = args[0];
-            System.out.println(s);
-            System.out.println(new Conversor().relativoParaAbsoluto(s));
+            System.out.println("Relativo: " + s);
+            System.out.println("Absoluto: " + new Conversor().relativoParaAbsoluto(s));
         }
     }
 }
